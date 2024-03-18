d90443e46075ba39e888dba528305a97fc624571
HCM00026\bp_SyNV <Sy.Nguyen@nttdata.com>
2024-03-18 17:03:33 +0700

    no message

---
+++ admin/furniture_admin/src/service/InformationOrderService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const INFORMATION_ORDER_DETAIL_API_BASE_URL = "http://localhost:9036/order-detail";
+
+class InformationOrderService {
+
+    
+}
+
+const informationOrderService = new InformationOrderService();
+export default informationOrderService;
