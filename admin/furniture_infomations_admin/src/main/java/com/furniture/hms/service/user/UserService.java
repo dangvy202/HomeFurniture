47140a4c2d10d457cd0a9b0583ba52842e7a3392
HCM00026\bp_SyNV <Sy.Nguyen@nttdata.com>
2024-03-20 17:17:48 +0700

    not yet user edit

--- admin/furniture_infomations_admin/src/main/java/com/furniture/hms/service/user/UserService.java
+++ admin/furniture_infomations_admin/src/main/java/com/furniture/hms/service/user/UserService.java
@@ -1,6 +1,9 @@
 package com.furniture.hms.service.user;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -8,6 +11,7 @@ import org.springframework.util.CollectionUtils;
 
 import com.furniture.hms.constant.UserMessage;
 import com.furniture.hms.dto.result_data.ResultData;
+import com.furniture.hms.dto.user.UserRequest;
 import com.furniture.hms.dto.user.UserResponse;
 import com.furniture.hms.entity.User;
 import com.furniture.hms.mapper.user.UserMapper;
@@ -22,6 +26,28 @@ import lombok.extern.slf4j.Slf4j;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public ResultData<UserResponse> saveEditUserByEmail(UserRequest request) {
+	ResultData<UserResponse> userDetail = this.getDetailUserByEmail(request.getEmail());
+	if (userDetail.getStatus() != Boolean.FALSE && userDetail.getMessage() != UserMessage.NOT_FOUND) {
+//	    Instant createDate = Instant.now();
+//	    Instant updateDate = Instant.now();
+	    Date dateFormat = new SimpleDateFormat("yyyy-MM-dd").parse(request.getUpdateDate().toString());
+	    UserResponse userResponse = userDetail.getResultData();
+	    userResponse.setFirstName(request.getFirstName());
+	    userResponse.setLastName(request.getLastName());
+	    userResponse.setUserName(request.getUserName());
+	    userResponse.setEmail(request.getEmail());
+//	    userResponse.setPassword(request.getPassword());
+	    userResponse.setAddress(request.getAddress());
+	    userResponse.setBirthday(request.getBirthday());
+	    userResponse.setRole(request.getRole());
+	    userResponse.setNation(request.getNation());
+	    userResponse.setPhone(request.getPhone());
+	    userResponse.setUpdateDate(Instant.now());
+	}
+	return null;
+    }
 
     public ResultData<List<UserResponse>> getAllUser() {
 	List<UserResponse> response = new ArrayList<>();
