9f79b2d5bb175c877a46ca10b66e7ff7da1142dd
HCM00026\bp_SyNV <Sy.Nguyen@nttdata.com>
2024-03-28 17:11:02 +0700

    wishlist be success

---
+++ admin/furniture_authentication_cms/src/main/java/com/furniture/hms/dto/wishlist/WishlistOrderCMSResponse.java
@@ -0,0 +1,62 @@
+package com.furniture.hms.dto.wishlist;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Setter
+@Getter
+public class WishlistOrderCMSResponse {
+
+    private User user;
+
+    private Product product;
+
+    @Data
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    public static class User {
+	@JsonProperty("email")
+	private String email;
+    }
+
+    @Data
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    public static class Product {
+
+	@JsonProperty("id")
+	private Integer id;
+
+	@JsonProperty("productName")
+	private String productName;
+
+	@JsonProperty("productPrice")
+	private BigDecimal productPrice;
+
+	@JsonProperty("productSaleoff")
+	private BigDecimal productSaleoff;
+
+	@JsonProperty("quantity")
+	private Integer quantity;
+
+	@JsonProperty("totalPrice")
+	private BigDecimal totalPrice;
+
+	private Picture picture;
+
+	@Data
+	@FieldDefaults(level = AccessLevel.PRIVATE)
+	public static class Picture {
+
+	    @JsonProperty("picture")
+	    private String picture;
+	}
+    }
+
+}
