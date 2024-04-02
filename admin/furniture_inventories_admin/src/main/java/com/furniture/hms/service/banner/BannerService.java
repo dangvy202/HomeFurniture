98845a43aa6851d3ba2af67872c1e6fcea498ee0
HCM00026\bp_SyNV <Sy.Nguyen@nttdata.com>
2024-04-02 17:00:33 +0700

    banner process

--- admin/furniture_inventories_admin/src/main/java/com/furniture/hms/service/banner/BannerService.java
+++ admin/furniture_inventories_admin/src/main/java/com/furniture/hms/service/banner/BannerService.java
@@ -1,14 +1,15 @@
 package com.furniture.hms.service.banner;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.furniture.hms.dto.banner.BannerResponse;
+import com.furniture.hms.dto.result_data.ResultData;
 import com.furniture.hms.entity.Banner;
-import com.furniture.hms.mapper.banner.BannerMapper;
 import com.furniture.hms.repository.banner.BannerRepository;
 
 import jakarta.transaction.Transactional;
@@ -20,19 +21,37 @@ public class BannerService {
 
     private final BannerRepository bannerRepository;
 
+    private static final String PATH_SRC_FRONTEND = "";
+
     @Transactional
-    public List<BannerResponse> getAllBanner() {
-	List<BannerResponse> response = new ArrayList<>();
+    public ResultData<BannerResponse> updateBanner(MultipartFile image, int id) {
+	Banner banner = bannerRepository.findById(id).orElse(null);
 
-	List<Banner> bannerList = bannerRepository.findAll();
-	if (!CollectionUtils.isEmpty(bannerList)) {
-	    bannerList.forEach(banner -> {
-		BannerResponse bannerResponse = BannerMapper.INSTANCE.toBannerRes(banner);
-		response.add(bannerResponse);
-	    });
+	if (banner != null) {
+	    try {
+
+		BufferedOutputStream outputStream = new BufferedOutputStream(
+			new FileOutputStream(new File(PATH_SRC_FRONTEND, image.getOriginalFilename())));
+	    } catch (Exception e) {
+		// TODO: handle exception
+	    }
 	}
-
-	return response;
+	return null;
     }
+
+//    @Transactional
+//    public List<BannerResponse> getAllBanner() {
+//	List<BannerResponse> response = new ArrayList<>();
+//
+//	List<Banner> bannerList = bannerRepository.findAll();
+//	if (!CollectionUtils.isEmpty(bannerList)) {
+//	    bannerList.forEach(banner -> {
+//		BannerResponse bannerResponse = BannerMapper.INSTANCE.toBannerRes(banner);
+//		response.add(bannerResponse);
+//	    });
+//	}
+//
+//	return response;
+//    }
 
 }
